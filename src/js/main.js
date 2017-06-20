a0f0f05bab847fcc684173b50619ce746041e683
LOEO <286269159@qq.com>
2017-06-20 23:53:47 +0800

    基础环境已经完成，可运行

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,10 @@
-var conf = require("../../conf.json");
+import conf from "../../conf.json";
+import "./testvue";
+import "../style/css/main.css"
 
 function main() {
     var ele = document.createElement("div");
     ele.textContent = conf.greetText;
     return ele;
 }
+//document.body.appendChild(main());
--- webpack-config-dev.js
+++ webpack-config-dev.js
@@ -1,37 +1,53 @@
 let path = require("path");
 let htmlWebpackPlugin = require("html-webpack-plugin");
+let webpack = require("webpack");
 module.exports = {
     entry: {
         main: "./src/js/main.js"
     },
     output: {
-        filename: "js/[name].js",
+        filename: "js/[name]-[hash].js",
         path: path.resolve(__dirname, "./dist")
     },
     devtool: "cheap-eval-source-map",
     plugins: [
         new htmlWebpackPlugin({
+            css:"./src/style/css/main.css",
             template: "./src/index.html",
-            title: "Test Vue"
+            title: "Test Vue",
+            output:"[name]-[hash].html"
         }),
         new webpack.HotModuleReplacementPlugin() //热加载插件
     ],
     devServer: {
         contentBase: "./dist",
-        colors: true,
         inline: true,
         historyApiFallback: true,
         hot: true
     },
     module: {
         loaders: [{
-                test: /\.json$/,
-                loader: "json-loader"
-            },
-            {
-                test: /\.css$/,
-                loader: "style!css"
+            test: /\.json$/,
+            loader: "json-loader"
+        },
+        {
+            test: /\.css$/,
+            loader: "style-loader!css-loader"
+        },
+        {
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: "babel-loader",
+            options: {
+                presets: ['env']
             }
+        }
+
         ]
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.common.js'
+        }
     }
 }
