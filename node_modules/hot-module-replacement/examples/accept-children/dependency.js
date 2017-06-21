eaf407f8e6769e7a3bfabde182ddec561e610347
LOEO <286269159@qq.com>
2017-06-21 10:21:46 +0800

    delete node_modules

--- node_modules/hot-module-replacement/examples/accept-children/dependency.js
+++
@@ -1,8 +0,0 @@
-const loadTimeTs = process.hrtime();
-const ns = loadTimeTs[0] * 1e9 + loadTimeTs[1];
-
-module.exports = function() {
-  process.send &&
-    process.send({ message: 'call from dependency', param: loadTimeTs });
-  return '';
-};
