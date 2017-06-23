3458812a3e5bd3ecc49776f2af04746ccd97d8cb
LOEO <286269159@qq.com>
2017-06-23 18:34:41 +0800

    add iview

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,28 @@ import conf from "../../conf.json";
 import "./testvue";
 import "../style/css/main.css"
 
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from 'components/app.vue';    // 路由挂载
+import Routers from './router.js';       // 路由列表
+import iView from 'iview';
+import 'iview/dist/styles/iview.css';    // 使用 CSS
+
+Vue.use(VueRouter);
+Vue.use(iView);
+
+// 路由配置
+const RouterConfig = {
+    routes: Routers
+};
+const router = new VueRouter(RouterConfig);
+
+new Vue({
+    el: '#app',
+    router: router,
+    render: h => h(App)
+});
+
 function main() {
     console.log(conf.greetText);
     console.log(2);
